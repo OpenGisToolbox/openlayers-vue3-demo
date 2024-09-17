4c159e831424475b952a095a9bcc68565c51e740
znlgis <znlgis@foxmail.com>
2024-09-17 17:45:13 +0800

    加入图层显示控制

--- src/components/DemoMap.vue
+++ src/components/DemoMap.vue
@@ -7,17 +7,25 @@
         :zoom="zoom"
     />
 
-    <Layers.OlLayerGroup v-for="group in dynamicLayerGroupList" :key="group.name" :title="group.name">
-      <Layers.OlTileLayer v-for="layer in group.layers" :key="layer.name" :title="layer.name">
-        <Sources.OlSourceTileWms :layers="layer.name" :url="layer.url" serverType="geoserver"/>
+    <Layers.OlLayerGroup v-for="group in dynamicLayerGroupList" :key="group.name" :title="group.name"
+                         :visible="group.visible">
+      <Layers.OlTileLayer v-for="layer in group.layers" :key="layer.name" :title="layer.name" :visible="layer.visible">
+        <Sources.OlSourceTileWms :layers="layer.name" :url="layer.url"/>
       </Layers.OlTileLayer>
     </Layers.OlLayerGroup>
 
-    <Layers.OlTileLayer v-for="layer in dynamicLayerList" :key="layer.name" :title="layer.name">
-      <Sources.OlSourceTileWms :layers="layer.name" :url="layer.url" serverType="geoserver"/>
+    <Layers.OlTileLayer v-for="layer in dynamicLayerList" :key="layer.name" :title="layer.name"
+                        :visible="layer.visible">
+      <Sources.OlSourceTileWms :layers="layer.name" :url="layer.url"/>
     </Layers.OlTileLayer>
 
-    <MapControls.OlLayerswitcherControl/>
+    <MapControls.OlLayerswitcherControl :collapsed="false"/>
+
+    <MapControls.OlZoomControl/>
+
+    <MapControls.OlContextMenuControl/>
+
+    <MapControls.OlScalelineControl/>
 
   </Map.OlMap>
 </template>
@@ -30,7 +38,7 @@ import GeoServerRestApi from '../geoserver/GeoServerRestApi';
 
 const center = ref([40, 40]);
 const projection = ref("EPSG:4326");
-const zoom = ref(8);
+const zoom = ref(0);
 const dynamicLayerList = ref([]);
 const dynamicLayerGroupList = ref([]);
 const mapRef = ref<MapRef | null>(null);
@@ -38,6 +46,26 @@ const mapRef = ref<MapRef | null>(null);
 onMounted(async () => {
   try {
     let geoServerRestApi = new GeoServerRestApi();
+
+    let layers = await geoServerRestApi.layers.getLayers();
+    let layerList = layers.layers.layer;
+    layerList.forEach((layer: any) => {
+      let layerName = layer.name;
+      if (layerName === 'ne:countries') {
+        dynamicLayerList.value.push({
+          name: layerName,
+          url: `/geoserver/wms`,
+          visible: true,
+        });
+      } else {
+        dynamicLayerList.value.push({
+          name: layerName,
+          url: `/geoserver/wms`,
+          visible: false,
+        });
+      }
+    });
+
     let groups = await geoServerRestApi.layers.getLayerGroups();
     for (let group of groups.layerGroups.layerGroup) {
       let groupName = group.name;
@@ -45,6 +73,7 @@ onMounted(async () => {
       let layerGroups = {
         name: groupName,
         layers: [],
+        visible: false,
       };
       groupLayers.layerGroup.publishables.published.forEach((layer: any) => {
         if (layer["@type"] !== "layer") return;
@@ -52,20 +81,11 @@ onMounted(async () => {
         layerGroups.layers.push({
           name: layer.name,
           url: `/geoserver/wms`,
+          visible: false,
         });
       });
       dynamicLayerGroupList.value.push(layerGroups);
     }
-
-    let layers = await geoServerRestApi.layers.getLayers();
-    let layerList = layers.layers.layer;
-    layerList.forEach((layer: any) => {
-      let layerName = layer.name;
-      dynamicLayerList.value.push({
-        name: layerName,
-        url: `/geoserver/wms`,
-      });
-    });
   } catch (error) {
     console.error("Failed to load layers:", error);
   }
